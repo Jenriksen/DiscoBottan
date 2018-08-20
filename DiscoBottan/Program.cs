0045bae9b97891a1f394531b642af4a3ffb72c0d
Jesper <jesperhen89@gmail.com>
2018-08-20 21:58:12 +0200

    Add project files.

---
+++ DiscoBottan/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace DiscoBottan
+{
+    class Program
+    {
+        private static DiscordClient _discord;
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            _discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "NDc3OTI0NTA4NzIwOTU1NDAz.DlDN1Q.-yAj4VY12STJDypGhm30-AunpIs",
+                TokenType = TokenType.Bot
+            });
+
+            _discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            await _discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
