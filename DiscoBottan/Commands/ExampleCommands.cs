c071994d0dbfdada28a8ddb2a1c1d890d934c5a9
Jesper <jesperhen89@gmail.com>
2018-10-03 08:58:13 +0200

    Added Greet method to ExampleCommands

--- DiscoBottan/Commands/ExampleCommands.cs
+++ DiscoBottan/Commands/ExampleCommands.cs
@@ -26,6 +26,25 @@ namespace DiscoBottan.Commands
             await ctx.RespondAsync($"{emoji} Hello, {member.Mention}");
         }
         
+        [Command("greet"), Description("Says hi to specified user."), Aliases("sayhi", "say_hi")]
+        public async Task Greet(CommandContext ctx, [Description("The user to say hi to.")] DiscordMember member) // this command takes a member as an argument; you can pass one by username, nickname, id, or mention
+        {
+            // note the [Description] attribute on the argument.
+            // this will appear when people invoke help for the
+            // command.
+
+            // let's trigger a typing indicator to let
+            // users know we're working
+            await ctx.TriggerTypingAsync();
+
+            // let's make the message a bit more colourful
+            var emoji = DiscordEmoji.FromName(ctx.Client, ":wave:");
+
+            // and finally, let's respond and greet the user.
+            await ctx.RespondAsync($"{emoji} Hello, {member.Mention}!");
+        }
+
+        
         [Command("sum"), Description("Sums all given numbers and returns said sum.")]
         public async Task SumOfNumbers(CommandContext ctx, [Description("Integers to sum.")] params int[] args)
         {
