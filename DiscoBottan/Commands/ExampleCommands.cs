69ab5ed0298b6eecaeb4ee47edea79cb4a69f039
Jesper <jesperhen89@gmail.com>
2018-10-03 13:38:05 +0200

    Added Interactive Commands.

--- DiscoBottan/Commands/ExampleCommands.cs
+++ DiscoBottan/Commands/ExampleCommands.cs
@@ -12,7 +12,7 @@ namespace DiscoBottan.Commands
     public class ExampleUngrouppedCommands
     {
         [Command("Ping")] // Method defining as a command
-        [DSharpPlus.CommandsNext.Attributes.Description("Example ping command")]
+        [Description("Example ping command")]
         [Aliases("pong")]
         // alternative names
         public async Task Ping(CommandContext ctx)
@@ -24,8 +24,8 @@ namespace DiscoBottan.Commands
             await ctx.RespondAsync($"{emoji} Pong! Ping: {ctx.Client.Ping}ms");
         }
 
-        [Command("greet"), DSharpPlus.CommandsNext.Attributes.Description("Says hi to specified user."), Aliases("sayhi", "say_hi")]
-        public async Task Greet(CommandContext ctx, [DSharpPlus.CommandsNext.Attributes.Description("The user to say hi to.")]
+        [Command("greet"), Description("Says hi to specified user."), Aliases("sayhi", "say_hi")]
+        public async Task Greet(CommandContext ctx, [Description("The user to say hi to.")]
             DiscordMember member) // this command takes a member as an argument; you can pass one by username, nickname, id, or mention
         {
             // note the [Description] attribute on the argument.
@@ -44,8 +44,8 @@ namespace DiscoBottan.Commands
         }
 
 
-        [Command("sum"), DSharpPlus.CommandsNext.Attributes.Description("Sums all given numbers and returns said sum.")]
-        public async Task SumOfNumbers(CommandContext ctx, [DSharpPlus.CommandsNext.Attributes.Description("Integers to sum.")] params int[] args)
+        [Command("sum"), Description("Sums all given numbers and returns said sum.")]
+        public async Task SumOfNumbers(CommandContext ctx, [Description("Integers to sum.")] params int[] args)
         {
             // triggers typing indicator to show that we are working.
             await ctx.TriggerTypingAsync();
@@ -57,10 +57,10 @@ namespace DiscoBottan.Commands
             await ctx.RespondAsync($"The sum of these numbers is {sum.ToString("#,##0")}");
         }
 
-        [Command("math"), DSharpPlus.CommandsNext.Attributes.Description("Does basic math.")]
-        public async Task Math(CommandContext ctx, [DSharpPlus.CommandsNext.Attributes.Description("Operation to perform on the operands.")]
-            MathOperation operation, [DSharpPlus.CommandsNext.Attributes.Description("First operand.")] double num1,
-            [DSharpPlus.CommandsNext.Attributes.Description("Second operand.")] double num2)
+        [Command("math"), Description("Does basic math.")]
+        public async Task Math(CommandContext ctx, [Description("Operation to perform on the operands.")]
+            MathOperation operation, [Description("First operand.")] double num1,
+            [Description("Second operand.")] double num2)
         {
             var result = 0.0;
 
@@ -90,15 +90,15 @@ namespace DiscoBottan.Commands
 
 
     [Group("admin")] // marks a command group
-    [DSharpPlus.CommandsNext.Attributes.Description("Administrative commands.")]
+    [Description("Administrative commands.")]
     [Hidden] //hides it from curious users
     [RequirePermissions(Permissions.ManageGuild)]
     // restricts usage to appropriate permission level.
     public class ExampleGrouppedCommands
     {
-        [Command("sudo"), DSharpPlus.CommandsNext.Attributes.Description("Executes a command as another user."), Hidden, RequireOwner]
-        public async Task Sudo(CommandContext ctx, [DSharpPlus.CommandsNext.Attributes.Description("Member to execute as.")] DiscordMember member,
-            [RemainingText, DSharpPlus.CommandsNext.Attributes.Description("Command text to execute.")]
+        [Command("sudo"), Description("Executes a command as another user."), Hidden, RequireOwner]
+        public async Task Sudo(CommandContext ctx, [Description("Member to execute as.")] DiscordMember member,
+            [RemainingText, Description("Command text to execute.")]
             string command)
         {
             await ctx.TriggerTypingAsync();
@@ -110,9 +110,9 @@ namespace DiscoBottan.Commands
             await cmds.SudoAsync(member, ctx.Channel, command);
         }
 
-        [Command("nick"), DSharpPlus.CommandsNext.Attributes.Description("Gives someone a new nickname."), RequirePermissions(Permissions.ManageNicknames)]
-        public async Task ChangeNickname(CommandContext ctx, [DSharpPlus.CommandsNext.Attributes.Description("Member to change the nickname for.")]
-            DiscordMember member, [RemainingText, DSharpPlus.CommandsNext.Attributes.Description("The nivkname to give that user.")]
+        [Command("nick"), Description("Gives someone a new nickname."), RequirePermissions(Permissions.ManageNicknames)]
+        public async Task ChangeNickname(CommandContext ctx, [Description("Member to change the nickname for.")]
+            DiscordMember member, [RemainingText, Description("The nivkname to give that user.")]
             string newNickname)
         {
             await ctx.TriggerTypingAsync();
@@ -137,7 +137,7 @@ namespace DiscoBottan.Commands
 
     [Group("memes", CanInvokeWithoutSubcommand =
         true)] // this makes the class a group, but with a twist; the class now needs an ExecuteGroupAsync method
-    [DSharpPlus.CommandsNext.Attributes.Description("Contains some memes. When invoked without subcommand, returns a random one.")]
+    [Description("Contains some memes. When invoked without subcommand, returns a random one.")]
     [Aliases("copypasta")]
     public class ExampleExecutableGroup
     {
@@ -167,7 +167,7 @@ namespace DiscoBottan.Commands
             }
         }
 
-        [Command("pepe"), Aliases("feelsbadman"), DSharpPlus.CommandsNext.Attributes.Description("Feels bad, man.")]
+        [Command("pepe"), Aliases("feelsbadman"), Description("Feels bad, man.")]
         public async Task Pepe(CommandContext ctx)
         {
             await ctx.TriggerTypingAsync();
@@ -181,7 +181,7 @@ namespace DiscoBottan.Commands
             await ctx.RespondAsync(embed: embed);
         }
 
-        [Command("navyseal"), Aliases("gorillawarfare"), DSharpPlus.CommandsNext.Attributes.Description("What the fuck did you just say to me?")]
+        [Command("navyseal"), Aliases("gorillawarfare"), Description("What the fuck did you just say to me?")]
         public async Task NavySeal(CommandContext ctx)
         {
             await ctx.TriggerTypingAsync();
@@ -189,7 +189,7 @@ namespace DiscoBottan.Commands
                 "What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo.");
         }
 
-        [Command("kekistani"), Aliases("kek", "normies"), DSharpPlus.CommandsNext.Attributes.Description("I'm a proud ethnic Kekistani.")]
+        [Command("kekistani"), Aliases("kek", "normies"), Description("I'm a proud ethnic Kekistani.")]
         public async Task Kekistani(CommandContext ctx)
         {
             await ctx.TriggerTypingAsync();
@@ -215,7 +215,7 @@ namespace DiscoBottan.Commands
                 await ctx.RespondAsync(embed: embed);
             }
 
-            [Command("ukip"), DSharpPlus.CommandsNext.Attributes.Description("The UKIP pledge.")]
+            [Command("ukip"), Description("The UKIP pledge.")]
             public async Task Ukip(CommandContext ctx)
             {
                 await ctx.TriggerTypingAsync();
@@ -229,7 +229,7 @@ namespace DiscoBottan.Commands
                 await ctx.RespondAsync(embed: embed);
             }
 
-            [Command("lineofsight"), DSharpPlus.CommandsNext.Attributes.Description("Line of sight.")]
+            [Command("lineofsight"), Description("Line of sight.")]
             public async Task Los(CommandContext ctx)
             {
                 await ctx.TriggerTypingAsync();
@@ -243,7 +243,7 @@ namespace DiscoBottan.Commands
                 await ctx.RespondAsync(embed: embed);
             }
 
-            [Command("art"), DSharpPlus.CommandsNext.Attributes.Description("Art.")]
+            [Command("art"), Description("Art.")]
             public async Task Art(CommandContext ctx)
             {
                 await ctx.TriggerTypingAsync();
@@ -257,7 +257,7 @@ namespace DiscoBottan.Commands
                 await ctx.RespondAsync(embed: embed);
             }
 
-            [Command("seeameme"), DSharpPlus.CommandsNext.Attributes.Description("When you see a meme.")]
+            [Command("seeameme"), Description("When you see a meme.")]
             public async Task SeeMeme(CommandContext ctx)
             {
                 await ctx.TriggerTypingAsync();
@@ -271,7 +271,7 @@ namespace DiscoBottan.Commands
                 await ctx.RespondAsync(embed: embed);
             }
 
-            [Command("thisis"), DSharpPlus.CommandsNext.Attributes.Description("This is meme man.")]
+            [Command("thisis"), Description("This is meme man.")]
             public async Task ThisIs(CommandContext ctx)
             {
                 await ctx.TriggerTypingAsync();
@@ -285,7 +285,7 @@ namespace DiscoBottan.Commands
                 await ctx.RespondAsync(embed: embed);
             }
 
-            [Command("deepdream"), DSharpPlus.CommandsNext.Attributes.Description("Deepdream'd meme man.")]
+            [Command("deepdream"), Description("Deepdream'd meme man.")]
             public async Task DeepDream(CommandContext ctx)
             {
                 await ctx.TriggerTypingAsync();
@@ -299,7 +299,7 @@ namespace DiscoBottan.Commands
                 await ctx.RespondAsync(embed: embed);
             }
 
-            [Command("sword"), DSharpPlus.CommandsNext.Attributes.Description("Meme with a sword?")]
+            [Command("sword"), Description("Meme with a sword?")]
             public async Task Sword(CommandContext ctx)
             {
                 await ctx.TriggerTypingAsync();
@@ -313,7 +313,7 @@ namespace DiscoBottan.Commands
                 await ctx.RespondAsync(embed: embed);
             }
 
-            [Command("christmas"), DSharpPlus.CommandsNext.Attributes.Description("Beneath the christmas spike...")]
+            [Command("christmas"), Description("Beneath the christmas spike...")]
             public async Task ChristmasSpike(CommandContext ctx)
             {
                 await ctx.TriggerTypingAsync();
---
+++ DiscoBottan/Commands/ExampleInteractiveCommands.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+
+namespace DiscoBottan.Commands
+{
+    public class ExampleInteractiveCommands
+    {
+        [Command("poll"), Description("Run a poll with reactions.")]
+        public async Task Poll(CommandContext ctx, [Description("How long should the poll last.")]
+            TimeSpan duration, [Description("What options should people have.")]
+            params DiscordEmoji[] options)
+        {
+            // Loads interactivity module and sets poll_options
+            var interactivity = ctx.Client.GetInteractivityModule();
+            var poll_options = options.Select(xe => xe.ToString());
+            
+            // presentation of poll
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = "Poll time!",
+                Description = string.Join(" ", poll_options)
+            };
+            
+            // add reactions as options
+            for (var i = 0; i < options.Length; i++)
+            {
+                await.msg.CreateReactionAsync(options[i]);
+            }
+            
+            // collect and filter responses
+            var poll_result = await interactivity.CollectReactionsAsync(msg, duration);
+            var results = poll_result.Where(xkvp => options.Contains(xkvp.Key))
+                .Select(xkvp => $"{xkvp.Key}: {xkvp.Value}");
+            
+            // post results
+            await ctx.RespondAsync(string.Join("\n", results));
+        }
+
+        [Command("waitforcode"), Description("Waits for a response containing a generated code.")]
+        public async Task WaitForCode(CommandContext ctx)
+        {
+            var interactivity = ctx.Client.GetInteractivityModule();
+            
+            // Generate a code
+            var codebytes = new byte[8];
+            using (var rng = RandomNumberGenerator.Create())
+                rng.GetBytes(codebytes);
+            var code = BitConverter.ToString(codebytes).ToLower().Replace("-", "");
+            
+            // announcing code
+            await ctx.RespondAsync($"The first one to type the following code gets a rewards: `{code}`");
+            
+            // Wait for anyone who types it
+            var msg = await interactivity.WaitForMessageAsync(xm => xm.Content.Contains(code),
+                TimeSpan.FromSeconds(60));
+            if (msg != null)
+            {
+                // announcing winner
+                await ctx.RespondAsync($"And the winner is: {msg.Message.Author.Mention}");
+            }
+            else
+            {
+                await ctx.RespondAsync("Nobody? Really?");
+            }
+        }
+
+        [Command("waitforreact"), Description("Waits for a reaction.")]
+        public async Task WaitForReaction(CommandContext ctx)
+        {
+            var interactivity = ctx.Client.GetInteractivityModule();
+
+            var emoji = DiscordEmoji.FromName(ctx.Client, ":point_up:");
+            
+            // wait for a reaction
+            var em = await interactivity.WaitForReactionAsync(xe => xe == emoji, ctx.User, TimeSpan.FromSeconds(60));
+            if (em != null)
+            {
+                // quote
+                var embed = new DiscordEmbedBuilder
+                {
+                    Color = em.Message.Author is DiscordMember m ? m.Color : new DiscordColor(0xFF00FF),
+                    Description = em.Message.Content,
+                    Author = new DiscordEmbedBuilder.EmbedAuthor
+                    {
+                        Name = em.Message.Author is DiscordMember mx ? mx.DisplayName : em.Message.Author.Username,
+                        IconUrl = em.Message.Author.AvatarUrl
+                    }
+                };
+                await ctx.RespondAsync(embed: embed);
+            }
+            else
+            {
+                await ctx.RespondAsync("Seriously?");
+            }
+        }
+    }
+}
